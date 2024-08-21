19dd01d825ca597e266bac2f9991961c3121e73a
pautri <Paul.Trilsbeek@mpi.nl>
2024-08-21 15:36:30 +0200

    Update AccessLevelProcessor.php
    index values as tokens

--- flat_permissions/src/Plugin/search_api/processor/AccessLevelProcessor.php
+++ flat_permissions/src/Plugin/search_api/processor/AccessLevelProcessor.php
@@ -3,8 +3,11 @@
 namespace Drupal\flat_permissions\Plugin\search_api\processor;
 
 use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\search_api\Index\Token;
 use Drupal\search_api\IndexInterface;
 use Drupal\search_api\Item\FieldInterface;
+use Drupal\search_api\Plugin\search_api\data_type\value\TextValue;
+use Drupal\search_api\Plugin\search_api\data_type\value\TextToken;
 
 /**
  * Provides a 'Access Level' processor.
@@ -18,64 +21,70 @@ use Drupal\search_api\Item\FieldInterface;
  *   },
  * )
  */
-class AccessLevelProcessor extends ProcessorPluginBase {
+class AccessLevelProcessor extends ProcessorPluginBase
+{
 
-    /**
+  /**
    * {@inheritdoc}
    */
-  public static function supportsIndex(IndexInterface $index) {
+  public static function supportsIndex(IndexInterface $index)
+  {
 
     return TRUE;
-
-}
+  }
 
 
   /**
    * {@inheritdoc}
    */
-  public function alterIndexedItems(array &$items) {
+  public function alterIndexedItems(array &$items)
+  {
+
+    ddm('alterIndexedItems');
     foreach ($items as $item) {
-              $field = $item->getField('field_read_access_policy');
-            if ($field) {
+      $field = $item->getField('field_read_access_policy');
+      if ($field) {
 
-              $this->processReadAccessPolicyField($field);
+        $this->processReadAccessPolicyField($field);
 
-              $item->setField('field_read_access_policy', $field);
-
-            }
-          }
-
+        $item->setField('field_read_access_policy', $field);
+      }
     }
+  }
 
   /**
-   * Process the Read Access Policy field before it is indexed.
+   * Process the Read Access Policy field before it is indexed. The field needs to be configured 
+   * in the search api coniguration as a "Fulltext Tokens" field.
    *
    * @param \Drupal\search_api\Item\FieldInterface $field
    *   The field to process.
    */
-  protected function processReadAccessPolicyField(FieldInterface &$field) {
+  protected function processReadAccessPolicyField(FieldInterface &$field)
+  {
+
     foreach ($field->getValues() as &$value) {
 
-    if (is_string($value)) {
+      if (is_object($value)) {
 
         $modified_value = $this->get_access_levels($value);
 
-        $modified_values[] = $modified_value;
+        $value->setTokens($modified_value);
 
+        $modified_values[] = $value;
       } else {
 
         $modified_values[] = $value;
-
       }
 
-     $field->setValues($modified_values);
+      $field->setValues($modified_values);
     }
   }
 
   /**
    * Function to retrieve the access levels from the access policy
    */
-  protected function get_access_levels($policy) {
+  protected function get_access_levels($policy)
+  {
 
     $levels = [];
 
@@ -84,46 +93,48 @@ class AccessLevelProcessor extends ProcessorPluginBase {
     $policy = json_decode($policy);
 
     if ($policy) {
-        if (property_exists($policy, "all")) {
-            $rule = $policy->all;
-            if (property_exists($rule, "roles")) {
-                $allowed_roles = $rule->roles;
-                $levels[] = $manager->rolesToLevel($allowed_roles);
-            } else {
-                $levels[] = 'restricted';
-            }
+      if (property_exists($policy, "all")) {
+        $rule = $policy->all;
+        if (property_exists($rule, "roles")) {
+          $allowed_roles = $rule->roles;
+          $levels[] = $manager->rolesToLevel($allowed_roles);
+        } else {
+          $levels[] = 'Restricted';
         }
+      }
 
-        if (property_exists($policy, "types")) {
-            $type_rules = $policy->types;
-            foreach ($type_rules as $type_rule) {
-                if (property_exists($type_rule, "roles")) {
-                    $allowed_roles = $type_rule->roles;
-                    $levels[] = $manager->rolesToLevel($allowed_roles);
-                } else {
-                    $levels[] = 'restricted';
-                }
-            }
+      if (property_exists($policy, "types")) {
+        $type_rules = $policy->types;
+        foreach ($type_rules as $type_rule) {
+          if (property_exists($type_rule, "roles")) {
+            $allowed_roles = $type_rule->roles;
+            $levels[] = $manager->rolesToLevel($allowed_roles);
+          } else {
+            $levels[] = 'Restricted';
+          }
         }
+      }
 
-        if (property_exists($policy, "files")) {
-            $files_rules = $policy->files;
-            foreach ($files_rules as $file_rule) {
-                if (property_exists($file_rule, "roles")) {
-                    $allowed_roles = $file_rule->roles;
-                    $levels[] = $manager->rolesToLevel($allowed_roles);
-                } else {
-                    $levels[] = 'restricted';
-                }
-            }
+      if (property_exists($policy, "files")) {
+        $files_rules = $policy->files;
+        foreach ($files_rules as $file_rule) {
+          if (property_exists($file_rule, "roles")) {
+            $allowed_roles = $file_rule->roles;
+            $levels[] = $manager->rolesToLevel($allowed_roles);
+          } else {
+            $levels[] = 'Restricted';
+          }
         }
+      }
     }
 
     $levels = array_unique($levels);
 
-    $levels = implode(',', $levels);
+    foreach ($levels as $level) {
+      // Create a new TextToken object for each level.
+      $level_tokens[] = new TextToken($level, 1);
+    }
 
-    return $levels;
-
-}
+    return $level_tokens;
+  }
 }
